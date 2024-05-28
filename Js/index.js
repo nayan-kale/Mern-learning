3bd5bc0fd2664edf03ab6a5ae817523f5d5c2fe0
nayan-kale <grad.nayankale@gmail.com>
2024-05-28 18:56:11 +0530

    star assignment

---
+++ Js/index.js
@@ -0,0 +1,15 @@
+//for(let i = 0; i < 4; i++) {
+ //let str = "";
+ //for(let j = 1; j < 5; j++) {
+   // str += "* ";
+// }
+ //console.log(str);
+//} 
+
+for(let i = 4; i >= 1; i--) {
+  let str ="";
+ for(let j = i;j>= 1; j--){
+   str+= "* "
+ }
+ console.log(str)
+}
