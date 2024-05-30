e9cab4c85f03fc1a59745736543f36268470f561
nayan-kale <grad.nayankale@gmail.com>
2024-05-30 17:52:07 +0530

    star pattern

--- Js/index.js
+++ Js/index.js
@@ -6,31 +6,41 @@
  //console.log(str);
 //} 
 
-for(let i = 4; i >= 1; i--) 
-  {
-  let str ="";
- for(let j = i;j>= 1; j--){
-   str+= "* "
- }
- console.log(str);
-}
+//for(let i = 4; i >= 1; i--) 
+  //{
+  //let str ="";
+ //for(let j = i;j>= 1; j--){
+   //str+= "* "
+ //}
+ //console.log(str);
+//}
 
-let n=5;
-for(let i =1; i<=n;i++)
-  {
-    let str=" ";
-    let space=" ";
+//let n=5;
+//for(let i =1; i<=n;i++)
+  //{
+   // let str=" ";
+    //let space=" ";
 
-    for(let j=0;j<(n-i);j++)
-      {
-      space+= '  ';
-    }
+    //for(let j=0;j<(n-i);j++)
+      //{
+      //space+= '  ';
+    //}
 
-    for(let k=0; k<i; k++)
-      {
-        str= str+'* ';
-      }
+   // for(let k=0; k<i; k++)
+     // {
+      //  str= str+'* ';
+      //}
 
-      console.log(space + str);
+      //console.log(space + str);
 
-}
+//}
+
+//for(let i = 0; i < 5; i++){
+  //let str = "";
+  //for(let j=1;j<=i;j++){
+   // str +=j +" ";
+  //}
+  //console.log(str);
+  //}
+
+
