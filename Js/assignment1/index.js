cad16a0033344ff8cfe931276ec990e887edea2c
nayan-kale <grad.nayankale@gmail.com>
2024-06-14 19:06:00 +0530

    home work

--- Js/assignment1/index.js
+++ Js/assignment1/index.js
@@ -31,33 +31,85 @@
 
 
 
-var amazon =[
-{name:"speaker",price :5400,rating : 4},
-{name:"headphones",price:3000,rating:3},
-{name:"playstation",price:20000,rating:5}
-];
+//var amazon =[
+//{name:"speaker",price :5400,rating : 4},
+//{name:"headphones",price:3000,rating:3},
+//{name:"playstation",price:20000,rating:5}
+//];
 
-for(let product of amazon){
-  console.log("name:",product.name,"rating:",product.rating)
-} 
-let total = 0
-for (let product of amazon){
-  total += product.rating
-}
+//for(let product of amazon){
+ // console.log("name:",product.name,"rating:",product.rating)
+//} 
+//let total = 0
+//for (let product of amazon){
+//  total += product.rating
+//}
 
-console.log("average rating of products is:", total/amazon.length)
-
-
-var minprice
-var minpriceproduct
-
-for(let product of amazon){
-  if(minprice == undefined || minprice > product.price){
-    minprice = product.price
-    minpriceproduct = product.name
-
-  }
-}
+//console.log("average rating of products is:", total/amazon.length)
 
 
-console.log ("product with minimum price is :", minpriceproduct)
+//var minprice
+//var minpriceproduct
+
+//for(let product of amazon){
+ // if(minprice == undefined || minprice > product.price){
+  //  minprice = product.price
+   // minpriceproduct = product.name
+
+  //}
+//}
+
+
+//console.log ("product with minimum price is :", minpriceproduct)
+
+
+//var e_commerce =[
+  //{name : "rice" ,quntity :2, price :60},
+  //{name :"dal" ,quntity :3,  price :50},
+  //{name :"salt" ,quntity :1, price :20},
+//];
+//total:function() {
+ // var total=0
+ // for(let i=0;i<Product.length;i++){
+  //  totalvalue += quntity[i] * price[i]
+ // }
+  //return totalvalue;
+//}
+//console.log(e_commerce.total_price())
+
+
+//var numbers = [1,2,3,4,5,6,7,8];
+//numbers.forEach(function(element){
+//console.log(element*element);
+//});
+
+
+//let arr = [1,2,3,4];
+//let double = arr.map(function(num){
+//  return (2*num)
+//})
+
+//console.log (double)
+
+
+
+//let odd = num.filter(function(elem){
+  //return elem%2 == 1
+
+//})
+ // console.log(odd)
+
+
+//let product = [1,2,3,4]. reduce(function(acc,elem){
+//  return acc * elem
+//},1)
+
+//console.log(product)
+
+//let odd = [1,2,3,4].filter(function(elem){ 
+// return elem%2 == 1 
+//}).reduce(function(acc,elem){
+ // return acc * elem
+//},1)
+
+//console.log(odd)
